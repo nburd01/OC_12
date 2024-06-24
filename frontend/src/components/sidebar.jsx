e239b653d727f6691a94fc5f39a1edf5664e6ea9
nburd01 <n.burdon@protonmail.com>
2024-06-24 17:26:49 +0200

    sidebar scss

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,11 +1,30 @@
+import { Link } from "react-router-dom";
 import "../styles/sidebar.scss";
+import weights from "../assets/weights.svg";
+import yoga from "../assets/yoga.svg";
+import swim from "../assets/swim.svg";
+import bike from "../assets/bike.svg";
+
 function Sidebar() {
   return (
     <div className="sidenav">
-      <a href="#">About</a>
-      <a href="#">Services</a>
-      <a href="#">Clients</a>
-      <a href="#">Contact</a>
+      <div className="icons">
+        <Link to="#">
+          <img src={yoga} alt="" />
+        </Link>
+        <Link to="#">
+          <img src={swim} alt="" />
+        </Link>
+        <Link to="#">
+          <img src={bike} alt="" />
+        </Link>
+        <Link to="#">
+          <img src={weights} alt="" />
+        </Link>
+      </div>
+      <div className="copyright">
+        <p>copyright</p>
+      </div>
     </div>
   );
 }
